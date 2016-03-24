237477c81102fea8e2d8ba11d2323fff7b73a0e3
Фролов Алексей <aleksey2093@outlook.com>
2016-03-24 23:20:35 +0300

    начало

---
+++ Chess_window/Chess_window/Main_window.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace Chess_window
+{
+    partial class Main_window
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1Основной = new System.Windows.Forms.TableLayoutPanel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tableLayoutPanel2Доска = new System.Windows.Forms.TableLayoutPanel();
+            this.новаяИграToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.локальнаяИграToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сетеваяИграToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.настройкиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.оПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.одинИгрокToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.дваИгрокаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.создатьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.подключитьсяToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableLayoutPanel1Основной.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1Основной
+            // 
+            this.tableLayoutPanel1Основной.ColumnCount = 1;
+            this.tableLayoutPanel1Основной.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1Основной.Controls.Add(this.menuStrip1, 0, 0);
+            this.tableLayoutPanel1Основной.Controls.Add(this.toolStrip1, 0, 2);
+            this.tableLayoutPanel1Основной.Controls.Add(this.tableLayoutPanel2Доска, 0, 1);
+            this.tableLayoutPanel1Основной.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1Основной.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1Основной.Name = "tableLayoutPanel1Основной";
+            this.tableLayoutPanel1Основной.RowCount = 3;
+            this.tableLayoutPanel1Основной.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1Основной.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1Основной.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1Основной.Size = new System.Drawing.Size(733, 334);
+            this.tableLayoutPanel1Основной.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.новаяИграToolStripMenuItem,
+            this.настройкиToolStripMenuItem,
+            this.оПрограммеToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(733, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Location = new System.Drawing.Point(0, 304);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(733, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // tableLayoutPanel2Доска
+            // 
+            this.tableLayoutPanel2Доска.AutoSize = true;
+            this.tableLayoutPanel2Доска.ColumnCount = 8;
+            this.tableLayoutPanel2Доска.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel2Доска.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel2Доска.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel2Доска.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel2Доска.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel2Доска.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel2Доска.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel2Доска.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel2Доска.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2Доска.Location = new System.Drawing.Point(3, 33);
+            this.tableLayoutPanel2Доска.Name = "tableLayoutPanel2Доска";
+            this.tableLayoutPanel2Доска.RowCount = 8;
+            this.tableLayoutPanel2Доска.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel2Доска.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel2Доска.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel2Доска.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel2Доска.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel2Доска.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel2Доска.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel2Доска.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel2Доска.Size = new System.Drawing.Size(727, 268);
+            this.tableLayoutPanel2Доска.TabIndex = 2;
+            // 
+            // новаяИграToolStripMenuItem
+            // 
+            this.новаяИграToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.локальнаяИграToolStripMenuItem,
+            this.сетеваяИграToolStripMenuItem});
+            this.новаяИграToolStripMenuItem.Name = "новаяИграToolStripMenuItem";
+            this.новаяИграToolStripMenuItem.Size = new System.Drawing.Size(81, 20);
+            this.новаяИграToolStripMenuItem.Text = "Новая игра";
+            // 
+            // локальнаяИграToolStripMenuItem
+            // 
+            this.локальнаяИграToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.одинИгрокToolStripMenuItem,
+            this.дваИгрокаToolStripMenuItem});
+            this.локальнаяИграToolStripMenuItem.Name = "локальнаяИграToolStripMenuItem";
+            this.локальнаяИграToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.локальнаяИграToolStripMenuItem.Text = "Локальная игра";
+            // 
+            // сетеваяИграToolStripMenuItem
+            // 
+            this.сетеваяИграToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.создатьToolStripMenuItem,
+            this.подключитьсяToolStripMenuItem});
+            this.сетеваяИграToolStripMenuItem.Name = "сетеваяИграToolStripMenuItem";
+            this.сетеваяИграToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.сетеваяИграToolStripMenuItem.Text = "Сетевая игра";
+            // 
+            // настройкиToolStripMenuItem
+            // 
+            this.настройкиToolStripMenuItem.Name = "настройкиToolStripMenuItem";
+            this.настройкиToolStripMenuItem.Size = new System.Drawing.Size(79, 20);
+            this.настройкиToolStripMenuItem.Text = "Настройки";
+            // 
+            // оПрограммеToolStripMenuItem
+            // 
+            this.оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
+            this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(94, 20);
+            this.оПрограммеToolStripMenuItem.Text = "О программе";
+            // 
+            // одинИгрокToolStripMenuItem
+            // 
+            this.одинИгрокToolStripMenuItem.Name = "одинИгрокToolStripMenuItem";
+            this.одинИгрокToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.одинИгрокToolStripMenuItem.Text = "Один игрок";
+            this.одинИгрокToolStripMenuItem.Click += new System.EventHandler(this.одинИгрокToolStripMenuItem_Click);
+            // 
+            // дваИгрокаToolStripMenuItem
+            // 
+            this.дваИгрокаToolStripMenuItem.Name = "дваИгрокаToolStripMenuItem";
+            this.дваИгрокаToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.дваИгрокаToolStripMenuItem.Text = "Два игрока";
+            this.дваИгрокаToolStripMenuItem.Click += new System.EventHandler(this.дваИгрокаToolStripMenuItem_Click);
+            // 
+            // создатьToolStripMenuItem
+            // 
+            this.создатьToolStripMenuItem.Name = "создатьToolStripMenuItem";
+            this.создатьToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.создатьToolStripMenuItem.Text = "Создать";
+            this.создатьToolStripMenuItem.Click += new System.EventHandler(this.создатьToolStripMenuItem_Click);
+            // 
+            // подключитьсяToolStripMenuItem
+            // 
+            this.подключитьсяToolStripMenuItem.Name = "подключитьсяToolStripMenuItem";
+            this.подключитьсяToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.подключитьсяToolStripMenuItem.Text = "Подключиться";
+            this.подключитьсяToolStripMenuItem.Click += new System.EventHandler(this.подключитьсяToolStripMenuItem_Click);
+            // 
+            // Main_window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(733, 334);
+            this.Controls.Add(this.tableLayoutPanel1Основной);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Main_window";
+            this.Text = "Шахматы";
+            this.Load += new System.EventHandler(this.Main_window_Load);
+            this.tableLayoutPanel1Основной.ResumeLayout(false);
+            this.tableLayoutPanel1Основной.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1Основной;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2Доска;
+        private System.Windows.Forms.ToolStripMenuItem новаяИграToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem локальнаяИграToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сетеваяИграToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem настройкиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem оПрограммеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem одинИгрокToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem дваИгрокаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem создатьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem подключитьсяToolStripMenuItem;
+    }
+}
+
---
+++ Chess_window/Chess_window/Main_window.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chess_window
+{
+    public partial class Main_window : Form
+    {
+        public Main_window()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_window_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 8; i++)
+                for (int j = 0; j < 8; j++)
+                {
+                    pic[i, j].Parent = (Control)tableLayoutPanel2Доска;
+                    pic[i, j].Dock = DockStyle.Fill;
+                    if ((i + j) % 2 == 0) { pic[i, j].BackColor = Color.White; }
+                    else { pic[i, j].BackColor = Color.Black; }
+                }
+        }
+
+        bool hod_now = true; //правда белые, ложь черные
+        bool gamer = true; //игрок играет за белые если правда, если ложь то за черные
+        PictureBox[,] pic = new PictureBox[8,8];
+        bool[,] doska = new bool[8, 8];
+
+        private void new_game_расстановка()
+        {
+            /*поле
+            0 1 2 3 4 5 6 7
+            8 9 10 11 12 13 14 15
+            16 17 18 19 20 21 22 23
+            24 25 26 27 28 29 30 31
+            32 33 34 35 36 37 38 39
+            40 41 42 43 44 45 46 47
+            48 49 50 51 52 53 54 55
+            56 57 58 59 60 61 62 63*/
+            //растановка черных
+            Bitmap a = new Bitmap(10,10, System.Drawing.Imaging.PixelFormat.Alpha);
+            a.SetPixel(1,1,Color.AliceBlue);
+        }
+
+        
+
+        private void одинИгрокToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("В настоящий момент одиночная игра недоступна", "Информация", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void дваИгрокаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            new_game_расстановка();
+        }
+
+        private void создатьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            new_game_расстановка();
+        }
+
+        private void подключитьсяToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            new_game_расстановка();
+        }
+    }
+}
